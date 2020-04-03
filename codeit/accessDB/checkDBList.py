ccfdf8e5b298a4a51dac8a075639c2a85a491a36
Partha Sarathi pandey <parthasarathi.x.pandey@gsk.com>
2020-04-03 07:25:52 +0530

    new file:   codeit/accessDB/checkDBList.py 	modified:   codeit/accessDB/mydb.py 	new file:   mysqlsampledatabase.sql

--- codeit/accessDB/checkDBList.py
+++ codeit/accessDB/checkDBList.py
@@ -1,10 +1,12 @@
-import mysql.connector
-
 mydb = mysql.connector.connect(
   host="35.200.219.211",
   user="partha",
   passwd="Partha@123"
-  database="classicmodels"
 )
 
-print(mydb)
+mycursor = mydb.cursor()
+
+mycursor.execute("SHOW DATABASES")
+
+for x in mycursor:
+  print(x)
