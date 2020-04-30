4eab3c4031283ec121f41d609b1d6b981a10d2c1
konda upendar <kondaupendar11@gmail.com>
2020-04-30 19:34:21 +0530

    fdljsdlkjf

---
+++ myprojects/whilebreakcont.py
@@ -0,0 +1,32 @@
+x=0
+
+while x < 5:
+    print(f'the value of x is: {x}')
+    x = x + 1
+else:
+    print('x is not less than 5')
+
+
+
+
+mystring='Upendar'
+for i in mystring:
+    if i == 'n':
+        continue
+    print(i)
+
+
+
+mystring='Upendar'
+for i in mystring:
+    if i == 'n':
+        break
+    print(i)
+
+
+
+for i in range(0,10,2):
+    print(i)
+print(list(range(0,10,2)))
+
+
