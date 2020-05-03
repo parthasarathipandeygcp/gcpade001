ce7194e3c0901680f01341045c7310057c4bef7d
parthasarathiwisdomin <parthasarathipandey@gmail.com>
2020-05-03 09:06:44 +0530

    On branch upendra

---
+++ Python180feet/oops/MethodOvveride.py
@@ -0,0 +1,43 @@
+# Run the codes in python prompt. It will not work here
+ #First copy the first class
+#Then copy the Second Class
+ #Theb run the below code lines one by one
+
+class Polygon:
+    def __init__(self, no_of_sides):
+        self.n = no_of_sides
+        self.sides = [0 for i in range(no_of_sides)]
+
+    def inputSides(self):
+        self.sides = [float(input("Enter side "+str(i+1)+" : ")) for i in range(self.n)]
+
+    def dispSides(self):
+        for i in range(self.n):
+            print("Side",i+1,"is",self.sides[i])
+
+class Triangle(Polygon):
+    def __init__(self):
+        Polygon.__init__(self,3)
+
+    def findArea(self):
+        a, b, c = self.sides
+        # calculate the semi-perimeter
+        s = (a + b + c) / 2
+        area = (s*(s-a)*(s-b)*(s-c)) ** 0.5
+        print('The area of the triangle is %0.2f' %area)
+
+
+t = Triangle()
+t.inputSides()
+t.dispSides()
+t.findArea()
+
+
+
+isinstance(t,Triangle)
+isinstance(t,Polygon)
+isinstance(t,int)
+isinstance(t,object)
+issubclass(Polygon,Triangle)
+issubclass(Triangle,Polygon)
+issubclass(bool,int)
